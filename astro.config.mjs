cc8f7d91c770e668e194a239b9d4450aa8f58ecc
Omar Salinas <osalinasv@outlook.com>
2023-09-04 09:28:38 -0600

    Add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind()]
-});
+  integrations: [tailwind(), react()],
+})
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,3 @@
-const colors = require('tailwindcss/colors')
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
