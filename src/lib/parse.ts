d4b363384931df4c5357ab192a0dee578ba02012
Omar Salinas <osalinasv@outlook.com>
2023-09-05 20:42:49 -0600

    Parsing ids into byte arrays

--- src/lib/parse.ts
+++ src/lib/parse.ts
@@ -1,3 +1,5 @@
+import { number } from 'astro/zod'
+import { parse as uuidParse } from 'uuid'
 import { version as uuidVersion } from 'uuid'
 import { validate as uuidValidate } from 'uuid'
 
@@ -27,6 +29,62 @@ export type IdFormat = {
   value: string
   segments: string[]
 }
+
+export function convertId(rawId: RawId) {
+  return rawId.type === 'uuid' ? convertFromUuid(rawId) : convertFromOracle(rawId)
+}
+
+function convertFromUuid(rawId: RawId) {
+  console.log('convertFromUuid', rawId.value)
+  const bytes = uuidParse(rawId.value)
+  const hex = Array.from(bytes.values(), (b) => b.toString(16).padStart(2, '0'))
+
+  console.log(hex)
+}
+
+function convertFromOracle(rawId: RawId) {
+  console.log('convertFromOracle', rawId.value)
+
+  const oracleBytes = getBytesFromOracle(rawId.value)
+  const uuidBytes = flipByteOrder(oracleBytes)
+
+  const bytes = new Uint8Array(uuidBytes)
+  const hex = Array.from(bytes.values(), (b) => b.toString(16).padStart(2, '0'))
+
+  console.log(hex)
+}
+
+function getBytesFromOracle(id: string) {
+  const octets = new Array<number>()
+  for (var i = 0; i < id.length; i += 2) {
+    octets.push(parseInt(id.substring(i, i + 2), 16))
+  }
+
+  return octets
+}
+
+function flipByteOrder(bytes: number[]) {
+  const newBytes = new Array<number>(16)
+
+  assignFlippedBytesInRange(newBytes, bytes, 0, 4)
+  assignFlippedBytesInRange(newBytes, bytes, 4, 6)
+  assignFlippedBytesInRange(newBytes, bytes, 6, 8)
+
+  for (let index = 8; index < bytes.length; index++) {
+    newBytes[index] = bytes[index]
+  }
+
+  return newBytes
+}
+
+function assignFlippedBytesInRange(target: number[], source: number[], start: number, end: number) {
+  for (let index = start; index < end; index++) {
+    target[index] = source[start + end - 1 - index]
+  }
+}
+
+// 00cb4112-0a70-4baa-82cd-e06d6166ddfa
+// 1241CB00700AAA4B82CDE06D6166DDFA
 
 // const bytes = uuidParse(rawValue)
 // const hex = Array.from(bytes.values(), (b) => b.toString(16).padStart(2, '0'))
