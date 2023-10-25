921e364209f85df8a4b2671e1c63a81e1dddd2e5
Omar Salinas <osalinasv@outlook.com>
2023-10-24 19:29:17 -0600

    Toggle per-type fieldsets and fix parameter mapping in generate

--- src/lib/uuid/generate.ts
+++ src/lib/uuid/generate.ts
@@ -4,12 +4,12 @@ import { flipNetworkByteOrder } from './shared'
 export const GENERATE_COUNT_MIN = 1
 export const GENERATE_COUNT_MAX = 50
 
-export const IdType = {
+export const ID_TYPE = {
   UUID: 'uuid',
   ORACLE: 'oracle',
 } as const
 
-type IdType = (typeof IdType)[keyof typeof IdType]
+export type IdType = (typeof ID_TYPE)[keyof typeof ID_TYPE]
 
 export type GenerateOptions = {
   count: number
