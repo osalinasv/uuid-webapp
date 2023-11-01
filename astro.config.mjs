95b27530e27932f592230eeb2943a3561cce0a3b
Omar Salinas <osalinasv@outlook.com>
2023-10-31 20:30:16 -0600

    Use @astrojs/node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import nodejs from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: nodejs({ mode: 'standalone' }),
   integrations: [tailwind({ applyBaseStyles: false })],
 })
