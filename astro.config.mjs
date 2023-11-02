d71c8821a758beaba571197a4de09361f64f220b
Omar Salinas <osalinasv@outlook.com>
2023-11-02 07:11:27 -0600

    Add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-import nodejs from '@astrojs/node'
+
+// import nodejs from '@astrojs/node'
+import netlify from '@astrojs/netlify/functions'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: nodejs({ mode: 'standalone' }),
+  // adapter: nodejs({ mode: 'standalone' }),
+  adapter: netlify(),
   integrations: [tailwind({ applyBaseStyles: false })],
 })
