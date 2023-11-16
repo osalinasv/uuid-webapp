92db8ce81089b4c5af6d4eb73f56fad85b05fb3d
Omar Salinas <osalinasv@outlook.com>
2023-11-16 09:50:23 -0700

    Use netlify functions per route

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import netlify from '@astrojs/netlify/functions'
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  site: 'https://uuid.osalinasv.dev/',
-  adapter: netlify(),
+  adapter: netlify({ functionPerRoute: true }),
   integrations: [tailwind({ applyBaseStyles: false })],
 })
