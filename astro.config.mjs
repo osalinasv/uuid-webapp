dc6c54d2b9e82c431a621066f210b8be7553913b
Omar Salinas <osalinasv@outlook.com>
2023-11-16 10:31:16 -0700

    Fix accessibility issues on radio and checkbox

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import netlify from '@astrojs/netlify/functions'
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  site: 'https://uuid.osalinasv.dev/',
   adapter: netlify({ functionPerRoute: false }),
   integrations: [tailwind({ applyBaseStyles: false })],
 })
