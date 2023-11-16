d8cc33334d8074f5cb1bbc7a5eecfe473d7c8274
Omar Salinas <osalinasv@outlook.com>
2023-11-15 20:34:38 -0700

    Update node version to LTS 20

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
-// import nodejs from '@astrojs/node'
 import netlify from '@astrojs/netlify/functions'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  // adapter: nodejs({ mode: 'standalone' }),
+  site: 'https://uuid.osalinasv.dev/',
   adapter: netlify(),
   integrations: [tailwind({ applyBaseStyles: false })],
 })
