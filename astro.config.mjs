74c5ca210fd696ac5a0aeaea2e8afcb360e6e0b4
Omar Salinas <osalinasv@outlook.com>
2023-12-21 21:22:53 -0700

    Update Astro to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
+import netlify from '@astrojs/netlify'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
-
-import netlify from '@astrojs/netlify/functions'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  site: 'https://uuid.osalinasv.dev/',
-  adapter: netlify({ functionPerRoute: false }),
+  adapter: netlify(),
   integrations: [tailwind({ applyBaseStyles: false })],
 })
